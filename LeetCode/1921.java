06388cafaec41c2c582f517d30390b7fa05b9ee9
jordan <54329371+jordansprojects@users.noreply.github.com>
2023-11-06 17:57:11 -0800

    Create 1921.java

---
+++ LeetCode/1921.java
@@ -0,0 +1,36 @@
+/*
+You are playing a video game where you are defending your city from a group of n monsters. You are given a 0-indexed integer array dist of size n, where dist[i] is the initial distance in kilometers of the ith monster from the city.
+
+The monsters walk toward the city at a constant speed. The speed of each monster is given to you in an integer array speed of size n, where speed[i] is the speed of the ith monster in kilometers per minute.
+
+You have a weapon that, once fully charged, can eliminate a single monster. However, the weapon takes one minute to charge. The weapon is fully charged at the very start.
+
+You lose when any monster reaches your city. If a monster reaches the city at the exact moment the weapon is fully charged, it counts as a loss, and the game ends before you can use your weapon.
+
+Return the maximum number of monsters that you can eliminate before you lose, or n if you can eliminate all the monsters before they reach the city.
+
+
+*/
+
+class Solution {
+    public int eliminateMaximum(int[] dist, int[] speed) {
+        int n = dist.length;
+        ArrayList<Float> eta = new ArrayList<>(dist.length);
+        // evaluate the monsters estimated time of arrival
+        for(int i = 0 ; i < dist.length; i++){
+            eta.add((float)(dist[i])/(float)(speed[i]));
+        }
+
+        Collections.sort(eta);
+
+        // simulate the ticking of minutes 
+        for(int i = 0 ; i < n; i++){
+            if (eta.get(i) <= i ){ // if the monster arrives before the next minute can pass it is too late
+                return i;
+            }
+        }
+        
+        return n;
+        
+    }
+}
